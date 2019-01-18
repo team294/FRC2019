74fa8f39bbe0db318ea93c6887250b657a828eef
BCR <bcr@rbusd.org>
2019-01-17 20:32:39 -0800

    Fixing robotPrefs
    Added comments, set to default

--- src/main/java/frc/robot/Utilities/RobotPreferences.java
+++ src/main/java/frc/robot/Utilities/RobotPreferences.java
@@ -28,7 +28,7 @@ public class RobotPreferences {
 	 */
 	public void refresh() {
 		inBCRLab = prefs.getBoolean("inBCRLab", false);
-		prototypeRobot = prefs.getBoolean("prototypeRobot", true); // true if testing code on a prototype, default to false (competition bot)
+		prototypeRobot = prefs.getBoolean("prototypeRobot", false); // true if testing code on a prototype, default to false (competition bot w/ Victors)
 		driveDirection = prefs.getBoolean("driveDirection", true);
 		wheelCircumference = prefs.getDouble("wheelDiameter", 6) * Math.PI;		
 		driveTrainDistanceFudgeFactor = prefs.getDouble("driveTrainDistanceFudgeFactor", 1);
