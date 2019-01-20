64f421f208363f8a1189223e87654805961af963
Dark General of Programming <arucane.wizard@gmail.com>
2019-01-20 13:23:33 -0800

    Vision reformatting update before pull request
    Changed control method for vision driving and command names to reflect purposes

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -65,13 +65,15 @@ public class OI {
         left[i].whenPressed(new Shift(false));
         right[i].whenPressed(new Shift(true));
       } else if (i == 3) {
-        left[i].whileHeld(new DriveWithVisionSteering());
-        right[i].whileHeld(new DriveWithVisionTurn());
+        left[i].whenPressed(new DriveWithVision());
+        left[i].whenReleased(new DriveWithJoysticks());
+        right[i].whenPressed(new VisionTurnToTarget());
+        right[i].whenReleased(new DriveWithJoysticks()); // We should be able to cancel the commands when the button is released. This is a better method to do that.
       }
     }
 
     
-    SmartDashboard.putData("Turn To Target", new DriveWithVisionTurn());
+    SmartDashboard.putData("Turn To Target", new VisionTurnToTarget());
 
     SmartDashboard.putData("Turn To Line", new TurnToLine());
     SmartDashboard.putBoolean("Left LineFollower", Robot.lineFollowing.isLinePresent(1));
--- src/main/java/frc/robot/commands/VisionTurnToTarget.java
+++ src/main/java/frc/robot/commands/VisionTurnToTarget.java
@@ -10,8 +10,8 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
-public class DriveWithVisionSteering extends Command {
-  public DriveWithVisionSteering() {
+public class VisionTurnToTarget extends Command {
+  public VisionTurnToTarget() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
     requires(Robot.driveTrain);
@@ -20,13 +20,13 @@ public class DriveWithVisionSteering extends Command {
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
-    Robot.driveTrain.driveToCrosshair();
+    Robot.driveTrain.turnToCrosshair();
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.driveTrain.driveToCrosshair();
+    Robot.driveTrain.turnToCrosshair();
   }
 
   // Make this return true when this Command no longer needs to run execute()
