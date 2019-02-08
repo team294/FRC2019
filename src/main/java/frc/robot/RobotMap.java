80dc193298e58b8e74da21f99912649f332fc9b6
Rob Coe <robcoe18@gmail.com>
2019-02-08 13:32:44 -0800

    Updated which motors are talon controlled

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,9 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
 
 package frc.robot;
 
@@ -32,8 +26,8 @@ public class RobotMap {
   public static final int cargoMotor2 = 41;
 
   public static final int climbMotor1 = 50;     // Talon
-  public static final int climbMotor2 = 51;
-  public static final int climbVacuum1 = 52;    // Talon
+  public static final int climbMotor2 = 51;     // Talon
+  public static final int climbVacuum1 = 52;
   public static final int climbVacuum2 = 53;
 
   public static final int wristMotor = 60;      // Talon
