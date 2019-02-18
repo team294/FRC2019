755cae6fe1caafc412be32b9b280d11958e2edab
Darren Ng <34221751+Darrengn@users.noreply.github.com>
2019-02-17 16:30:15 -0800

    Fixed stop cargo intake

--- src/main/java/frc/robot/commands/CargoStop.java
+++ src/main/java/frc/robot/commands/CargoStop.java
@@ -8,27 +8,31 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
 
 public class CargoStop extends Command {
   public CargoStop() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
+    requires(Robot.cargo);
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+    Robot.cargo.stopCargoIntake();
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    Robot.cargo.stopCargoIntake();
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return true;
   }
 
   // Called once after isFinished returns true
