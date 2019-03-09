d5350023c72bf4b17be52cf21dce51b1daa01608
Darren Ng <jennamt1282@gmail.com>
2019-03-09 15:04:30 -0800

    Changed everything on driveTrain to be compatible with SparkMax

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -5,9 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-
 package frc.robot.subsystems;   // had to change from just frc.robot.subsystems ?
-
 
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -20,8 +18,6 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.kauailabs.navx.frc.AHRS;
 
-
-
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -31,25 +27,34 @@ import frc.robot.commands.DriveWithJoysticks;
 import frc.robot.utilities.FileLog;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.I2C;
+
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-
 
 /**
  * Drive Train subsystem.  
  */
 public class DriveTrain extends Subsystem {
-  private final BaseMotorController leftMotor1;
-  private final WPI_TalonSRX leftMotor2 = new WPI_TalonSRX(RobotMap.leftMotor2);
-  private final BaseMotorController leftMotor3;
-  private final BaseMotorController rightMotor1;
-  private final WPI_TalonSRX rightMotor2 = new WPI_TalonSRX(RobotMap.rightMotor2);
-  private final BaseMotorController rightMotor3;
+  // private final BaseMotorController leftMotor1;
+  // private final WPI_TalonSRX leftMotor2 = new WPI_TalonSRX(RobotMap.leftMotor2);
+  // private final BaseMotorController leftMotor3;
+  // private final BaseMotorController rightMotor1;
+  // private final WPI_TalonSRX rightMotor2 = new WPI_TalonSRX(RobotMap.rightMotor2);
+  // private final BaseMotorController rightMotor3;
+
+  private CANSparkMax leftMotor1 = new CANSparkMax(RobotMap.leftMotor1, MotorType.kBrushless);
+  private CANSparkMax leftMotor2 = new CANSparkMax(RobotMap.leftMotor2, MotorType.kBrushless);
+  private CANSparkMax leftMotor3 = new CANSparkMax(RobotMap.leftMotor3, MotorType.kBrushless);
+  private CANSparkMax rightMotor1 = new CANSparkMax(RobotMap.rightMotor1, MotorType.kBrushless);
+  private CANSparkMax rightMotor2 = new CANSparkMax(RobotMap.rightMotor2, MotorType.kBrushless);
+  private CANSparkMax rightMotor3 = new CANSparkMax(RobotMap.rightMotor3, MotorType.kBrushless);
+  
+  private CANEncoder leftDriveEnc;
+  private CANEncoder rightDriveEnc;
 
   public final DifferentialDrive robotDrive = new DifferentialDrive(leftMotor2, rightMotor2);
-
-  private final CANSparkMax testMotor = new CANSparkMax(/**deviceID here*/, /**brushed or brushless here */);
 
   // Gyro variables
   private AHRS ahrs;
@@ -67,32 +72,33 @@ public class DriveTrain extends Subsystem {
 
   public DriveTrain() {
 
-    // Reads the robot prefs to check if using Victors or Talons for alternate motor controllers
-    if (Robot.robotPrefs.prototypeRobot) { // true means Talons
-      leftMotor1 = new WPI_TalonSRX(RobotMap.leftMotor1);
-      leftMotor3 = new WPI_TalonSRX(RobotMap.leftMotor3);
-      rightMotor1 = new WPI_TalonSRX(RobotMap.rightMotor1);
-      rightMotor3 = new WPI_TalonSRX(RobotMap.rightMotor3);
+    // // Reads the robot prefs to check if using Victors or Talons for alternate motor controllers
+    // if (Robot.robotPrefs.prototypeRobot) { // true means Talons
+    //   leftMotor1 = new WPI_TalonSRX(RobotMap.leftMotor1);
+    //   leftMotor3 = new WPI_TalonSRX(RobotMap.leftMotor3);
+    //   rightMotor1 = new WPI_TalonSRX(RobotMap.rightMotor1);
+    //   rightMotor3 = new WPI_TalonSRX(RobotMap.rightMotor3);
 
-      leftMotor1.set(ControlMode.Follower, RobotMap.leftMotor2);
-      leftMotor3.set(ControlMode.Follower, RobotMap.leftMotor2);
-      rightMotor1.set(ControlMode.Follower, RobotMap.rightMotor2);
-      rightMotor3.set(ControlMode.Follower, RobotMap.rightMotor2);
-    } else {
-      leftMotor1 = new WPI_VictorSPX(RobotMap.leftMotor1);
-      leftMotor3 = new WPI_VictorSPX(RobotMap.leftMotor3);
-      rightMotor1 = new WPI_VictorSPX(RobotMap.rightMotor1);
-      rightMotor3 = new WPI_VictorSPX(RobotMap.rightMotor3);
+    //   leftMotor1.set(ControlMode.Follower, RobotMap.leftMotor2);
+    //   leftMotor3.set(ControlMode.Follower, RobotMap.leftMotor2);
+    //   rightMotor1.set(ControlMode.Follower, RobotMap.rightMotor2);
+    //   rightMotor3.set(ControlMode.Follower, RobotMap.rightMotor2);
+    // } else {
+    //   leftMotor1 = new WPI_VictorSPX(RobotMap.leftMotor1);
+    //   leftMotor3 = new WPI_VictorSPX(RobotMap.leftMotor3);
+    //   rightMotor1 = new WPI_VictorSPX(RobotMap.rightMotor1);
+    //   rightMotor3 = new WPI_VictorSPX(RobotMap.rightMotor3);
 
-      leftMotor1.follow(leftMotor2);
-      leftMotor3.follow(leftMotor2);
-      rightMotor1.follow(rightMotor2);
-      rightMotor3.follow(rightMotor2);
-    }
+    //   leftMotor1.follow(leftMotor2);
+    //   leftMotor3.follow(leftMotor2);
+    //   rightMotor1.follow(rightMotor2);
+    //   rightMotor3.follow(rightMotor2);
+    // }
+    
+    leftDriveEnc = leftMotor2.getEncoder();
+    rightDriveEnc = rightMotor2.getEncoder();
 
-    leftMotor2.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
-		rightMotor2.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
-		zeroLeftEncoder();
+    zeroLeftEncoder();
 		zeroRightEncoder();
 
     leftMotor1.setInverted(true);
@@ -102,19 +108,19 @@ public class DriveTrain extends Subsystem {
     rightMotor2.setInverted(true);
     rightMotor3.setInverted(true);
 
-    leftMotor1.clearStickyFaults(0);
-    leftMotor2.clearStickyFaults(0);
-    leftMotor3.clearStickyFaults(0);
-    rightMotor1.clearStickyFaults(0);
-    rightMotor2.clearStickyFaults(0);
-    rightMotor3.clearStickyFaults(0);
+    leftMotor1.setIdleMode(IdleMode.kBrake);
+    leftMotor2.setIdleMode(IdleMode.kBrake);
+    leftMotor3.setIdleMode(IdleMode.kBrake);
+    rightMotor1.setIdleMode(IdleMode.kBrake);
+    rightMotor2.setIdleMode(IdleMode.kBrake);
+    rightMotor3.setIdleMode(IdleMode.kBrake);
 
-    leftMotor1.setNeutralMode(NeutralMode.Brake);
-    leftMotor2.setNeutralMode(NeutralMode.Brake);
-    leftMotor3.setNeutralMode(NeutralMode.Brake);
-    rightMotor1.setNeutralMode(NeutralMode.Brake);
-    rightMotor2.setNeutralMode(NeutralMode.Brake);
-    rightMotor3.setNeutralMode(NeutralMode.Brake);
+    leftMotor1.clearFaults();
+    leftMotor2.clearFaults();
+    leftMotor3.clearFaults();
+    rightMotor1.clearFaults();
+    rightMotor2.clearFaults();
+    rightMotor3.clearFaults();
 
     // Configure navX
 		try {
@@ -173,7 +179,7 @@ public class DriveTrain extends Subsystem {
 	 */
 	public void setLeftMotors(double powerPct) {
 		//TODO check if direction forward/backward is correct
-		leftMotor2.set(ControlMode.PercentOutput, -powerPct);
+		leftMotor2.set(-powerPct);
 	}
 
 	/**
@@ -183,7 +189,7 @@ public class DriveTrain extends Subsystem {
 	 */
 	public void setRightMotors(double powerPct) {
 		//TODO check if direction forward/backward is correct
-		rightMotor2.set(ControlMode.PercentOutput, powerPct);
+		rightMotor2.set(powerPct);
   }
 
   // Why does this exist? We have stop() for the same purpose. We don't need stopAllMotors()
@@ -203,26 +209,35 @@ public class DriveTrain extends Subsystem {
 	 * @param turnOn true=turn on, false= turn off
 	 */
 	public void setVoltageCompensation(boolean turnOn) {
-		leftMotor1.enableVoltageCompensation(turnOn);
-		leftMotor2.enableVoltageCompensation(turnOn);
-		leftMotor3.enableVoltageCompensation(turnOn);
-		rightMotor1.enableVoltageCompensation(turnOn);
-		rightMotor2.enableVoltageCompensation(turnOn);
-		rightMotor3.enableVoltageCompensation(turnOn);
+		if (turnOn) {
+      leftMotor1.enableVoltageCompensation(12.0);
+		  leftMotor2.enableVoltageCompensation(12.0);
+		  leftMotor3.enableVoltageCompensation(12.0);
+		  rightMotor1.enableVoltageCompensation(12.0);
+		  rightMotor2.enableVoltageCompensation(12.0);
+      rightMotor3.enableVoltageCompensation(12.0);
+    } else {
+      leftMotor1.disableVoltageCompensation();
+      leftMotor2.disableVoltageCompensation();
+      leftMotor3.disableVoltageCompensation();
+      rightMotor1.disableVoltageCompensation();
+      rightMotor2.disableVoltageCompensation();
+      rightMotor3.disableVoltageCompensation();
+    }
 	}
   
   /**
 	 * Zeros the left encoder position in software
 	 */
 	public void zeroLeftEncoder() {
-		leftEncoderZero = leftMotor2.getSelectedSensorPosition(0);
+		leftEncoderZero = leftDriveEnc.getPosition();
 	}
 
 	/**
 	 * Zeros the right encoder position in software
 	 */
 	public void zeroRightEncoder() {
-    rightEncoderZero = rightMotor2.getSelectedSensorPosition(0);
+    rightEncoderZero = rightDriveEnc.getPosition();
 	}
 
 	/**
@@ -231,7 +246,7 @@ public class DriveTrain extends Subsystem {
 	 * @return encoder position, in ticks
 	 */
 	public double getLeftEncoderTicks() {
-    return leftMotor2.getSelectedSensorPosition(0) - leftEncoderZero;
+    return leftDriveEnc.getPosition() - leftEncoderZero;
 	}
 
 	/**
@@ -240,7 +255,7 @@ public class DriveTrain extends Subsystem {
 	 * @return encoder position, in ticks
 	 */
 	public double getRightEncoderTicks() {
-		return -(rightMotor2.getSelectedSensorPosition(0) - rightEncoderZero);
+		return -(rightDriveEnc.getPosition() - rightEncoderZero);
 	}
 
   public double encoderTicksToInches(double encoderTicks) {
@@ -326,19 +341,19 @@ public class DriveTrain extends Subsystem {
    */
   public void setDriveMode(boolean setCoast){
    if (setCoast) {
-    leftMotor1.setNeutralMode(NeutralMode.Coast);
-    leftMotor2.setNeutralMode(NeutralMode.Coast);
-    leftMotor3.setNeutralMode(NeutralMode.Coast);
-    rightMotor1.setNeutralMode(NeutralMode.Coast);
-    rightMotor2.setNeutralMode(NeutralMode.Coast);
-    rightMotor3.setNeutralMode(NeutralMode.Coast);
+    leftMotor1.setIdleMode(IdleMode.kCoast);
+    leftMotor2.setIdleMode(IdleMode.kCoast);
+    leftMotor3.setIdleMode(IdleMode.kCoast);
+    rightMotor1.setIdleMode(IdleMode.kCoast);
+    rightMotor2.setIdleMode(IdleMode.kCoast);
+    rightMotor3.setIdleMode(IdleMode.kCoast);
    } else {
-    leftMotor1.setNeutralMode(NeutralMode.Brake);
-    leftMotor2.setNeutralMode(NeutralMode.Brake);
-    leftMotor3.setNeutralMode(NeutralMode.Brake);
-    rightMotor1.setNeutralMode(NeutralMode.Brake);
-    rightMotor2.setNeutralMode(NeutralMode.Brake);
-    rightMotor3.setNeutralMode(NeutralMode.Brake);
+    leftMotor1.setIdleMode(IdleMode.kBrake);
+    leftMotor2.setIdleMode(IdleMode.kBrake);
+    leftMotor3.setIdleMode(IdleMode.kBrake);
+    rightMotor1.setIdleMode(IdleMode.kBrake);
+    rightMotor2.setIdleMode(IdleMode.kBrake);
+    rightMotor3.setIdleMode(IdleMode.kBrake);
    }
    
   }
@@ -398,9 +413,9 @@ public class DriveTrain extends Subsystem {
     */
   public void updateDriveLog (boolean logWhenDisabled) {
     Robot.log.writeLog(logWhenDisabled, "DriveTrain", "Update Variables",
-      "Drive L1 Volts," + leftMotor1.getMotorOutputVoltage() + ",Drive L2 Volts," + leftMotor2.getMotorOutputVoltage() + ",Drive L3 Volts," + leftMotor3.getMotorOutputVoltage() +
+      "Drive L1 Volts," + leftMotor1.getBusVoltage() + ",Drive L2 Volts," + leftMotor2.getBusVoltage() + ",Drive L3 Volts," + leftMotor3.getBusVoltage() +
       ",Drive L1 Amps," + Robot.pdp.getCurrent(RobotMap.leftMotor1PDP) + ",Drive L2 Amps," + Robot.pdp.getCurrent(RobotMap.leftMotor2PDP) + ",Drive L3 Amps," + Robot.pdp.getCurrent(RobotMap.leftMotor3PDP) + 
-      ",Drive R1 Volts," + rightMotor1.getMotorOutputVoltage() + ",Drive R2 Volts," + rightMotor2.getMotorOutputVoltage() + ",Drive R3 Volts," + rightMotor3.getMotorOutputVoltage() + 
+      ",Drive R1 Volts," + rightMotor1.getBusVoltage() + ",Drive R2 Volts," + rightMotor2.getBusVoltage() + ",Drive R3 Volts," + rightMotor3.getBusVoltage() + 
       ",Drive R1 Amps," + Robot.pdp.getCurrent(RobotMap.rightMotor1PDP) + ",Drive R2 Amps," + Robot.pdp.getCurrent(RobotMap.rightMotor2PDP) + ",Drive R3 Amps," + Robot.pdp.getCurrent(RobotMap.rightMotor3PDP) + 
       ",L Enc Ticks," + getLeftEncoderTicks() + ",L Drive Inches," + getLeftEncoderInches() + 
       ",R Enc Ticks," + getRightEncoderTicks() + ",R Drive Inches," + getRightEncoderInches() + 
