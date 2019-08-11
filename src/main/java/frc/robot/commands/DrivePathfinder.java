cf093ea1a66efc68f0e5042af5b8dcab27ace3dd
BCR <BCR@rbusd.org>
2019-08-11 16:12:56 -0700

    Added skid adjustments for FF and FB

--- src/main/java/frc/robot/commands/DrivePathfinder.java
+++ src/main/java/frc/robot/commands/DrivePathfinder.java
@@ -13,6 +13,7 @@ import frc.robot.Robot;
 import frc.robot.pathfinder.Pathfinder;
 import frc.robot.pathfinder.Trajectory;
 import frc.robot.pathfinder.followers.DistanceFollower;
+import frc.robot.utilities.RobotPreferences;
 
 public class DrivePathfinder extends Command {
   private DistanceFollower dfLeft, dfRight, dfCenter;
@@ -25,10 +26,13 @@ public class DrivePathfinder extends Command {
                                                   // false = a path did not load, stop pathfinder
 
   double distL = 0, distR = 0, distC = 0;      // Distance traveled
+  double distLPrior = 0, distRPrior = 0, distCPrior = 0;      // Distance traveled in prior iteration of loop
   Trajectory.Segment segLeft, segRight, segCenter;
   double l = 0, r = 0, c = 0, distErrTerm = 0, turn = 0;    // power to send to drive motors
-  double gyro_heading = 0, desired_heading = 0;
-  
+  double gyroHeading = 0, gyroHeadingPrior = 0, desiredHeading = 0;
+  double skidGain = 0.28;      // Magnify delta between wheels by this factor to account for skid
+  double skidAdjust = 0.0;    // Temp variable for skid adjustments
+
   /**
    * Drive following a path
    * @param pathName        File name excluding .pf1.csv
@@ -85,6 +89,7 @@ public class DrivePathfinder extends Command {
     segLeft = dfLeft.getSegment();
     segRight = dfRight.getSegment();
 
+    // Prime string building in data logger to prevent delays when executing the command
     logData();
   }
 
@@ -113,12 +118,15 @@ public class DrivePathfinder extends Command {
     dfLeft.reset();
     dfRight.reset();
     dfCenter.reset();
-    distL = 0;
-    distR = 0;
-    distC = 0;
+    distL = 0;  distLPrior = 0;
+    distR = 0;  distRPrior = 0;
+    distC = 0;  distCPrior = 0;
 
     if (resetGyro) {
-      Robot.driveTrain.setGyroRotation(Pathfinder.r2d(trajCenter.segments[0].heading));
+      gyroHeadingPrior = Pathfinder.r2d(trajCenter.segments[0].heading);
+      Robot.driveTrain.setGyroRotation(gyroHeadingPrior);
+    } else {
+      gyroHeadingPrior = Robot.driveTrain.getGyroRotation();
     }
   }
 
@@ -130,40 +138,66 @@ public class DrivePathfinder extends Command {
       return;
     }
     
+    // Get distance travelled
     distL = Robot.driveTrain.getLeftEncoderInches();
     distR = Robot.driveTrain.getRightEncoderInches();
-    distC = (distL + distR) / 2;
 
+    // Calculate wheel power from left and right profiles
     l = dfLeft.calculate(distL);
     r = dfRight.calculate(distR);
     c = dfCenter.calculate(distC);
 
+    // Adjust feed-forward for wheel skid when turning
+    skidAdjust = (l-r)*skidGain/2.0;
+
+    // Get segments for data logging and feedback calculations
     segLeft = dfLeft.getSegment();
     segRight = dfRight.getSegment();
     segCenter = dfCenter.getSegment();
 
-    gyro_heading = Robot.driveTrain.getGyroRotation();    // Assuming the gyro is giving a value in degrees
-    desired_heading = Pathfinder.r2d(dfCenter.getHeading());  // Should also be in degrees
+    // Add feedback for error in robot heading
+    gyroHeading = Robot.driveTrain.getGyroRotation();    // Gyro heading in degrees
+    desiredHeading = Pathfinder.r2d(dfCenter.getHeading());  // Should also be in degrees
+    turn = 0.01 * Pathfinder.boundHalfDegrees(desiredHeading - gyroHeading);
 
+    // Add feedback for distance travelled
+    // distC = (distL + distR) / 2;
+    double deltaL = distL - distLPrior;
+    double deltaR = distR - distRPrior;
+    if (deltaL > deltaR) {
+      // Turning right.  Adjust for skid.
+      // Use the distance from the inner wheel from the turn + heading change
+      distC = distCPrior +  deltaR + Pathfinder.d2r(Pathfinder.boundHalfDegrees(gyroHeading - gyroHeadingPrior)) * Robot.robotPrefs.wheelbase_in / 2.0;
+    } else {
+      // Turning left.  Adjust for skid.
+      // Use the distance from the inner wheel from the turn + heading change
+      distC = distCPrior +  deltaL + Pathfinder.d2r(Pathfinder.boundHalfDegrees(gyroHeadingPrior - gyroHeading)) * Robot.robotPrefs.wheelbase_in / 2.0;
+    }
     distErrTerm = 0.08 * (segCenter.position - distC);
 
-    double angleDifference = Pathfinder.boundHalfDegrees(desired_heading - gyro_heading);
-    turn = 0.01 * angleDifference;
-
-    Robot.driveTrain.setLeftMotors(-(l + distErrTerm + turn));
-    Robot.driveTrain.setRightMotors(-(r + distErrTerm - turn));
+    // Set the motor percentage based on feed forward and feedback to follow the profile
+    Robot.driveTrain.setLeftMotors(-(l + skidAdjust + distErrTerm + turn));
+    Robot.driveTrain.setRightMotors(-(r + - skidAdjust + distErrTerm - turn));
     
     logData();
+
+    // Save distances and heading
+    distLPrior = distL;
+    distRPrior = distR;
+    distCPrior = distC;
+    gyroHeadingPrior = gyroHeading;
   }
 
-//TODO update log filing
   private void logData() {
     Robot.log.writeLog(false, "Pathfinder", "execute", "time," + ((double)(System.currentTimeMillis() - dfLeft.getStartTimeMillis())) / 1000.0 +
-    ",left power," + l + ",right power," + r + ",turn power," + turn +
-    ",left distance," + distL + ",right distance," + distR + ",left vel," + Robot.driveTrain.getLeftEncoderVelocity() + ",right vel," + Robot.driveTrain.getRightEncoderVelocity() +
-    ",heading," + gyro_heading + ",left isFinished," + dfLeft.isFinished() +
+    ",left power," + l + ",right power," + r + ",turn power," + turn + ",skid power," + skidAdjust + ",dist FB power," + distErrTerm +
+    ",left distance," + distL + ",right distance," + distR + ",center distance," + distC +
+    ",left vel," + Robot.driveTrain.getLeftEncoderVelocity() + ",right vel," + Robot.driveTrain.getRightEncoderVelocity() +
+    ",heading," + gyroHeading + ",center segPos," + segCenter.position +
+    ",left isFinished," + dfLeft.isFinished() +
     ",left segPos," + segLeft.position + ",left segVel," + segLeft.velocity + ",left segAccel," + segLeft.acceleration + 
-    ",left segJerk," + segLeft.jerk + ",left segHeading," + Pathfinder.boundHalfDegrees(Pathfinder.r2d(segLeft.heading)) + ",left segdt," + segLeft.dt + ",right isFinished," + dfRight.isFinished() + 
+    ",left segJerk," + segLeft.jerk + ",left segHeading," + Pathfinder.boundHalfDegrees(Pathfinder.r2d(segLeft.heading)) + ",left segdt," + segLeft.dt + 
+    ",right isFinished," + dfRight.isFinished() + 
     ",right segPos," + segRight.position + ",right segVel," + segRight.velocity + ",right segAccel," + segRight.acceleration + 
     ",right segJerk," + segRight.jerk + ",right segHeading," + Pathfinder.boundHalfDegrees(Pathfinder.r2d(segRight.heading)) + ",right segdt," + segRight.dt);
   }
